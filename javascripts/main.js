f6d8ec4c6f4c7562bbafbbbead4b41c1734d001a
FlorianBosval <Onibi@MacBook-Pro-de-Florian.local>
2014-12-18 17:30:08 +0100

    js test1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,33 @@
 console.log('This would be the main JS file.');
+
+jQuery.githubUser = function(username, callback) 
+{
+   jQuery.getJSON('https://api.github.com/repos/'+ username, callback);
+}
+ 
+jQuery.fn.loadRepositories = function(username) 
+{
+    this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
+     
+    var target = this;
+    $.githubUser(username, function(data) 
+    {
+        var repos = data.data;
+        sortByName(repos);   
+     
+        var list = $('<dl/>');
+        target.empty().append(list);
+        $(repos).each(function() {
+            if (this.name != (username.toLowerCase()+'.github.com')) {
+                list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
+                list.append('<dd>' + this.description +'</dd>');
+            }
+        });      
+      });
+      
+    function sortByName(repos) {
+        repos.sort(function(a,b) {
+        return a.name - b.name;
+       });
+    }
+};
